fc9d6270d62fadf1046bb5bf430e62f33e21472f
ISC <isc-robotics@umich.edu>
2023-01-18 22:30:55 -0500

    attempt to appease the potato

--- src/server.rs
+++ src/server.rs
@@ -2,15 +2,24 @@ use std::io::Write;
 use std::net::TcpListener;
 
 use opencv::core::Mat;
+use opencv::core::Size;
+use opencv::imgproc::INTER_LINEAR;
 use opencv::prelude::*;
 use opencv::videoio::CAP_ANY;
+use opencv::videoio::CAP_PROP_BUFFERSIZE;
+use opencv::videoio::CAP_PROP_FORMAT;
 
 fn main() -> anyhow::Result<()> {
     let server_ip: Vec<String> = std::env::args().take(2).collect();
 
-    let mut video = opencv::videoio::VideoCapture::new(0, CAP_ANY)?;
+    let mut video = opencv::videoio::VideoCapture::default()?;
+    video.set(CAP_PROP_BUFFERSIZE, 1.0)?;
+    video.set(CAP_PROP_FORMAT, -1.0)?;
+    video.open(0, CAP_ANY)?;
 
     let stream = TcpListener::bind(server_ip[1].clone())?;
+
+    println!("Init!");
 
     loop {
         let (mut remote, addr) = stream.accept()?;
@@ -20,17 +29,29 @@ fn main() -> anyhow::Result<()> {
         let mut buf = Mat::default();
         loop {
             if video.read(&mut buf)? {
+                let mut resize = Mat::default();
+
+                // Downsample image to half size
+                opencv::imgproc::resize(
+                    &mut buf,
+                    &mut resize,
+                    Size::default(),
+                    1.0,
+                    1.0,
+                    INTER_LINEAR,
+                )?;
+
                 // Transmit size on first connection
                 if is_first_trans {
-                    let rows = buf.rows().to_be_bytes();
-                    let cols = buf.cols().to_be_bytes();
+                    let rows = (resize.rows()).to_be_bytes();
+                    let cols = (resize.cols()).to_be_bytes();
 
                     remote.write_all(&rows)?;
                     remote.write_all(&cols)?;
                     is_first_trans = false;
                 }
 
-                let bytes = buf.data_bytes()?;
+                let bytes = resize.data_bytes()?;
 
                 if remote.write_all(bytes).is_err() {
                     println!("Connection broke, waiting for new connection...");
